d215e97ef53dbab9ec87762daab04e15172c07b8
Be-Smart <serhii.vladymyrov@gmail.com>
2016-11-12 18:06:42 +0200

    Class clock was added

--- app/clock/index.js
+++ app/clock/index.js
@@ -2,22 +2,52 @@
 
 import template from './clock.pug';
 import './clock.sass';
+import Timer from './timer';
 
 export default class Clock {
   constructor(el) {
     this._elem = el;
     this._elem.innerHTML = template();
-    this._elem.addEventListener('click', this._toggle.bind(this));
-    this._timerDisplay = this._elem.querySelector('[data-display="timer"]');
-    this._settingsDisplay = this._elem.querySelector('[data-display="settings"]');
+    this._counting = false;
+
+    // this._minutesElem = this._elem.querySelector('[data-element="minutes"]');
+    this._timer = new Timer({
+      minutesElem: this._elem.querySelector('[data-element="minutes"]'),
+      secondsElem: this._elem.querySelector('[data-element="seconds"]')
+    });
+
+    this._elem.addEventListener('click', this._onClockDisplayClick.bind(this));
   }
 
-  _toggle(event) {
-    let elem = event.target.closest('[data-element="btn"]');
-    let dataSet = event.target.dataset.btn;
-    if (!elem) return;
-    this._timerDisplay.classList.toggle('js-hidden');
-    this._settingsDisplay.classList.toggle('js-hidden');
+  _onClockDisplayClick(event) {
+    let btnElem = event.target.closest('[data-element="btn"]');
+    let unitsElem = event.target.closest('[data-element="units"]');
+
+    if (btnElem) {
+      this._toggleClockDisplay();
+    } else if (unitsElem) {
+      this._switchTimerState();
+    }
+  }
+
+  _toggleClockDisplay () {
+    let timerDisplay = this._elem.querySelector('[data-display="timer"]');
+    let settingsDisplay = this._elem.querySelector('[data-display="settings"]');
+
+    timerDisplay.classList.toggle('js-hidden');
+    settingsDisplay.classList.toggle('js-hidden');
+  }
+
+  _switchTimerState () {
+    let inputValue = this._elem.querySelector('#session').value;
+    console.log(inputValue);
+    if (!this._counting) {
+      this._timer.start(inputValue);
+      this._counting = true;
+    } else {
+      this._timer.stop();
+      this._counting = false;
+    }
   }
 
 }
