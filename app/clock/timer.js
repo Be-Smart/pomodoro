fae1328957ccef3603fd84e8d7f2e0c9eb3e1621
Be-Smart <serhii.vladymyrov@gmail.com>
2016-11-14 21:43:18 +0200

    Method render was added

--- app/clock/timer.js
+++ app/clock/timer.js
@@ -2,33 +2,34 @@
 
 export default class Timer {
   constructor(options) {
-    this._minutesElem = options.minutes;
-    this._secondsElem = options.seconds;
+    this._minutesElem = options.minutesElem;
+    this._secondsElem = options.secondsElem;
+    this._stateElem = options.stateElem;
+    this._isSession = true;
   }
 
-  start(minutes){
-    this._defaultTime = minutes;
-    let timeInSeconds = minutes * 60 -1;
+  start(sessionLength, pauseLength) {
+    this._sessionLength = sessionLength;
+    this._breakLength = pauseLength;
+    this._timeInSeconds = (this._isSession ? this._sessionLength : this._breakLength) * 60 - 1;
 
     this._timer = setInterval( () => {
-      let time = this._updateTime(timeInSeconds);
-
-      this._minutesElem.innerHTML = time.minutes;
-      this._secondsElem.innerHTML = time.seconds;
-
-      timeInSeconds--;
+      let time = this._updateTime(this._timeInSeconds);
+      this._render(time.minutes, time.seconds);
     }, 1000 );
-
   }
 
-  stop() {
+  stop(sessionLength) {
     clearInterval(this._timer);
-    this._minutesElem.innerHTML = this._defaultTime;
-    this._secondsElem.innerHTML = '00';
+    this._isSession = true;
+    this._render(sessionLength);
   }
 
   _updateTime(time) {
-    if (time < 1) clearInterval(this._timer);
+    if (time < 1) {
+      this._isSession = this._isSession ? false : true;
+      this._timeInSeconds = (this._isSession ? this._sessionLength : this._breakLength) * 60 + 1;
+    }
 
     let min = Math.floor( (time/60) % 60 );
     if (min < 10) min = `0${min}`;
@@ -36,10 +37,18 @@ export default class Timer {
     let sec = Math.floor(time % 60);
     if (sec < 10) sec = `0${sec}`;
 
+    this._timeInSeconds--;
+
     return {
       minutes: min,
       seconds: sec
     };
+  }
+
+  _render (minutes='00', seconds='00') {
+    this._minutesElem.innerHTML = minutes;
+    this._secondsElem.innerHTML = seconds;
+    this._stateElem.innerHTML = this._isSession ? 'Focus' : 'Break';
   }
 
 }
