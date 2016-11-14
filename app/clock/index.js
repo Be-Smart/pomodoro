8fec5d50b641a105e0f26ed23489af267318132a
Be-Smart <serhii.vladymyrov@gmail.com>
2016-11-14 22:44:35 +0200

    onInput eventListener was added

--- app/clock/index.js
+++ app/clock/index.js
@@ -10,14 +10,32 @@ export default class Clock {
     this._elem.innerHTML = template();
     this._counting = false;
 
-    // this._minutesElem = this._elem.querySelector('[data-element="minutes"]');
+    this._minutesElem = this._elem.querySelector('[data-element="minutes"]');
     this._timer = new Timer({
       minutesElem: this._elem.querySelector('[data-element="minutes"]'),
-      secondsElem: this._elem.querySelector('[data-element="seconds"]')
+      secondsElem: this._elem.querySelector('[data-element="seconds"]'),
+      stateElem: this._elem.querySelector('[data-element="state"]')
     });
 
     this._elem.addEventListener('click', this._onClockDisplayClick.bind(this));
+    this._elem.addEventListener('input', this._onInputValueChange.bind(this));
   }
+
+  _onInputValueChange (event) {
+    let inputElem = event.target.closest('[data-element="field"]');
+    let fieldValue = inputElem.value;
+
+    if ( isNaN(fieldValue) || fieldValue < 1 || fieldValue > 60 ) {
+      fieldValue = inputElem.value = 1;
+      console.log(fieldValue);
+    }
+
+    if (inputElem.id === 'session') {
+      if (fieldValue < 10) fieldValue = `0${fieldValue}`;
+      this._minutesElem.innerHTML = fieldValue;
+    }
+  }
+
 
   _onClockDisplayClick(event) {
     let btnElem = event.target.closest('[data-element="btn"]');
@@ -39,13 +57,17 @@ export default class Clock {
   }
 
   _switchTimerState () {
-    let inputValue = this._elem.querySelector('#session').value;
-    console.log(inputValue);
+    let sessionValue = this._elem.querySelector('#session').value;
+    if (sessionValue < 10) sessionValue = `0${sessionValue}`;
+
+    let breakValue = this._elem.querySelector('#break').value;
+    if (breakValue < 10) breakValue = `0${breakValue}`;
+
     if (!this._counting) {
-      this._timer.start(inputValue);
+      this._timer.start(sessionValue, breakValue);
       this._counting = true;
     } else {
-      this._timer.stop();
+      this._timer.stop(sessionValue);
       this._counting = false;
     }
   }
