5dd247f6525c7944a06ff93391289a3fe44659a3
Be-Smart <serhii.vladymyrov@gmail.com>
2016-11-16 21:39:12 +0200

    Refactoring

--- app/clock/index.js
+++ app/clock/index.js
@@ -3,6 +3,7 @@
 import template from './clock.pug';
 import './clock.sass';
 import Timer from './timer';
+import FieldController from './field-controller';
 
 export default class Clock {
   constructor(elem) {
@@ -11,61 +12,24 @@ export default class Clock {
     this._counting = false;
 
     this._minutesElem = this._elem.querySelector('[data-element="minutes"]');
+    this._stateElem = this._elem.querySelector('[data-element="state"]')
+
+    new FieldController ({
+      elem: this._elem,
+      minutesElem: this._minutesElem,
+      inputAttribute: '[data-element="field"]',
+      sessionFieldId: 'session'
+    });
+
     this._timer = new Timer({
-      minutesElem: this._elem.querySelector('[data-element="minutes"]'),
+      minutesElem: this._minutesElem,
       secondsElem: this._elem.querySelector('[data-element="seconds"]'),
-      stateElem: this._elem.querySelector('[data-element="state"]')
+      stateElem: this._stateElem
     });
 
     this._elem.addEventListener('click', this._onClockDisplayClick.bind(this));
-    this._elem.addEventListener('input', this._onInputValueChange.bind(this));
-    this._elem.addEventListener('keypress', this._onKeyPress.bind(this));
   }
 
-  //input handlers
-  _onKeyPress (event) {
-    if (event.ctrlKey || event.altKey || event.metaKey) return;
-
-    let char = this._getChar(event);
-
-    if (char == null) return;
-
-    if (char < '0' || char > '9') {
-      event.preventDefault();
-    }
-  }
-
-  _getChar (event) {
-    if (event.which == null) {
-      if (event.keyCode < 32) return null;
-      return String.fromCharCode(event.keyCode)
-    }
-
-    if (event.which != 0 && event.charCode != 0) {
-      if (event.which < 32) return null;
-      return String.fromCharCode(event.which);
-    }
-
-    return null;
-  }
-
-  _onInputValueChange (event) {
-    let inputElem = event.target.closest('[data-element="field"]');
-    let fieldValue = inputElem.value;
-
-    if (fieldValue < 1) {
-      fieldValue = inputElem.value = 1;
-    } else if (fieldValue > 60) {
-      fieldValue = inputElem.value = 60;
-    }
-
-    if (inputElem.id === 'session') {
-      if (fieldValue < 10) fieldValue = `0${fieldValue}`;
-      this._minutesElem.innerHTML = fieldValue;
-    }
-  }
-
-  //Click handlers
   _onClockDisplayClick(event) {
     let btnElem = event.target.closest('[data-element="btn"]');
     let unitsElem = event.target.closest('[data-element="units"]');
@@ -75,6 +39,7 @@ export default class Clock {
     } else if (unitsElem) {
       this._switchTimerState();
     }
+    this._stateElem.style.color = this._counting ? 'rgba(168, 210, 240, 0.9)' : '';
   }
 
   _toggleClockDisplay () {
